66f6a6542ba391dbdda6d94489dadc08bf3fa37f
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-12-02 18:54:18 +0000

    initial commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,22 @@
 const request = require('request');
+const args = process.argv.slice(2);
+
+const url = 'https://api.thecatapi.com/v1/breeds/search?name=' + args[0];
+
+request(url, function(error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the Status code
+
+  // console.log('url: ', url)
+  // console.log(typeof body);
+  // console.log('typeof data: ', typeof data);
+
+  const data = JSON.parse(body);
+
+  if (data[0]) {
+    console.log('description: ', data[0]['description']);
+  } else {
+    console.log('breed not found');
+  }
+});
+
